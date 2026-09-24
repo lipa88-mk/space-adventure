cec48b57434e893b612eb95a85e35383755fd96d
Maria Korshunova <po4taimenimenia@gmail.com>
2026-09-24 14:34:24 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="page">
+      <main className="page__main">
+        <h1>Space Adventure</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;

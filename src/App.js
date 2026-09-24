f060ea069b7b1b3d089c8f5b762acdb83a9aa362
Maria Korshunova <po4taimenimenia@gmail.com>
2026-09-24 18:41:14 +0200

    Add offers section with responsive styles and new button design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import './App.scss';
 import logoIcon from './assets/icons/logo_GO.svg';
 import cartIcon from './assets/icons/cart.svg';
 
+const offers = [
+  {
+    id: 1,
+    title: 'Move the borders of reality!',
+    text: "Go on a space adventure - it's possible with us!",
+    isWide: true,
+    details:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora neque eaque natus eligendi nemo maxime animi unde atque, illum dicta molestias veniam voluptas architecto a voluptatem officiis numquam nobis omnis.',
+  },
+  {
+    id: 2,
+    title: 'Space is not just stars and planets',
+    text: 'Go on a space adventure',
+    details:
+      'Lorem ipsum dolor sit amet conseco maxime animi unde atque, illum dicta molestias veniam voluptas architecto a voluptatem officiis numquam nobis omnis.',
+  },
+  {
+    id: 3,
+    title: 'For those who dream of stars',
+    text: 'Our offer: make your dream come true',
+    details:
+      'Tempora neque eaque natus eligendi nemo maxime animi unde atque, illum dicta molestias veniam voluptas architecto a voluptatem officiis numquam nobis omnis.',
+  },
+  {
+    id: 4,
+    title: 'Fulfill your fantastic dreams',
+    text: 'Space has never been so close',
+    isWide: true,
+    details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+];
+
 function App() {
   return (
     <div className="page">
@@ -68,6 +100,53 @@ function App() {
               Learn more
             </button>
           </div>
+        </section>
+
+        <section className="offers container">
+          <h2 className="offers__title">Offers</h2>
+
+          <ul className="offers__list">
+            {offers.map(({ id, title, text, isWide, details }) => (
+              <li
+                className={`offers__item${isWide ? ' offers__item--wide' : ''}`}
+                key={id}
+              >
+                <article
+                  className={`offer-card offer-card--${id}${
+                    isWide ? ' offer-card--wide' : ''
+                  }`}
+                >
+                  <h3 className="offer-card__title">{title}</h3>
+                  <p className="offer-card__text">{text}</p>
+
+                  <input
+                    className="offer-card__toggle visually-hidden"
+                    type="checkbox"
+                    id={`offer-toggle-${id}`}
+                    aria-controls={`offer-details-${id}`}
+                  />
+                  <label
+                    className="offer-card__button button"
+                    htmlFor={`offer-toggle-${id}`}
+                  >
+                    <span className="offer-card__button-text offer-card__button-text--more">
+                      Learn more
+                    </span>
+                    <span className="offer-card__button-text offer-card__button-text--less">
+                      Hide
+                    </span>
+                  </label>
+
+                  <div
+                    className="offer-card__details"
+                    id={`offer-details-${id}`}
+                  >
+                    <p className="offer-card__details-text">{details}</p>
+                  </div>
+                </article>
+              </li>
+            ))}
+          </ul>
         </section>
       </main>
       <footer className="footer"></footer>
